870e57a99c1719ac277308987f1102f5dc7bd0bf
Swathi Krishna <swathikrishna2627@gmail.com>
2022-03-27 01:53:09 +0530

    react movie

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1 className='align-items-center'>MOVIE APPLICATION</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
+
